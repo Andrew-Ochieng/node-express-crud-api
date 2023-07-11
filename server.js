879beb8d907dd883b561e7a007e4ff2ec3890c93
Code-X <andrewomosh44@gmail.com>
2023-07-11 08:13:59 +0300

    add delete request && code-refactoring

--- server.js
+++ server.js
@@ -1,56 +1,57 @@
 const express = require('express')
 
 const app = express()
-
-const users = require('./data/users.json')
-
 app.use(express.json())
 
+const data = require('./data/todos.json')
+const todos = data.todos
 
-// routes
+
+
 // get request
-app.get('/users', (req, res) => {
-    res.send({
-        success: true,
-        message: "Data fetched successfully",
-        data: users
-    })
+app.get('/todos', (req, res) => {
+    res.json(todos)
 })
 
-// get user of id
-// app.get(`/users/${users.id}`, (req, res) => {
-//     res.send({
-//         success: true,
-//         message: "Data fetched successfully",
-//         data: users
-//     })
-// })
+// get one todo
+app.get('/todos/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const todo = todos.find((item) => item.id === id)
 
+    !todo ? res.status(404).send('Todo not found') : res.json(todo)  
+})
 
 // post request
-app.post('/user', (req, res) => {
-    let name = req.body.name
-    let age = req.body.age
-    let location = req.body.location
-    users.push({
-        id: (users.length + 1).toString(),
-        name: name,
-        age: age,
-        location: location
-    })
-
-    res.send({
-        success: true,
-        message: "User added successfully"
-    })
+app.post('/todos', (req, res) => {
+    const newTodo = {
+        id: todos.length + 1,
+        title: req.body.title,
+        description: req.body.description
+    }
+    todos.push(newTodo)
+    res.json(newTodo)
 })
+
+// put request
+// app.put('/todos/:id')
+
+// delete request
+app.delete('/todos/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const todo = todos.findIndex((item) => item.id === id)
+
+    todo === -1 ? res.status(404).send('Todo not found') : res.status(200).json('Todo deleted successfully!')
+})
+
+
+
 
 
 
 
 
 app.listen(8000, () => {
-    console.log('Node app is runnning on port 8000!')
+    console.log('Todo app is runnning on port 8000!')
 })
 
 
